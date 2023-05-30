0a6a1a5baa63382f312f46619491f0b57104acb0
nkosithipe <88943092+nkosithipe@users.noreply.github.com>
2023-05-31 00:50:41 +0200

    Create a function that randomly return either 'rock', 'paper' or 'scissors'

---
+++ rock-paper-scissors-game.js
@@ -0,0 +1,8 @@
+const options = ['rock', 'paper', 'scissors'];
+
+//Function that Randomly picks an option as a computer choice/selection.
+function getComputerChoice(){
+    const computerOptions = Math.floor(Math.random() * options.length);
+    const computerChoice = options[computerOptions];
+    return computerChoice;
+}
