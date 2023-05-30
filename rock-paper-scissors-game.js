24e4c50adb4be9c0e1858a61518a0ab16d367adf
nkosithipe <88943092+nkosithipe@users.noreply.github.com>
2023-05-31 01:17:52 +0200

    Create function that takes 2 parameters and play a single round

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -6,3 +6,22 @@ function getComputerChoice(){
     const computerChoice = options[computerOptions];
     return computerChoice;
 }
+
+//Function for playing a single round which takes 2 parameters.
+function playRound(playerSelection, computerSelection){
+        
+    console.log(`Player chose ${playerSelection}`);
+    console.log(`Computer chose ${computerSelection}`);
+    
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||(playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'rock')){
+        console.log(`You Win! ${playerSelection} beats ${computerSelection} `);
+        playerScore++;
+    }else if (( computerSelection== 'rock' &&  playerSelection== 'scissors') || (computerSelection == 'scissors' && playerSelection == 'paper') ||
+    (computerSelection == 'paper' && playerSelection == 'rock')){
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}.`);
+        computerScore++;
+    }else{
+        console.log('It\'s a Tie!');
+        return 'Tie';
+    }
+}
